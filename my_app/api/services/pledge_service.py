2e0c19b12fbd2ff4c81d0f38232acb92d5c7f1e3
Juan Manuel Oubina <juan.oubina@rappi.com>
2021-06-05 18:01:19 -0300

    Add pledge type

--- my_app/api/services/pledge_service.py
+++ my_app/api/services/pledge_service.py
@@ -1,9 +1,9 @@
-from my_app.api.domain import PledgePrototype
+from my_app.api.domain import PledgePrototype, Pledge
 
 
 class PledgeService:
     def __init__(self, pledge_repository):
         self.pledge_repository = pledge_repository
 
-    def create_pledge(self, prototype: PledgePrototype):
+    def create_pledge(self, prototype: PledgePrototype) -> Pledge:
         return self.pledge_repository.create_pledge(prototype)
