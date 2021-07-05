0880ca767292e9066f349cd34a5aabb26a3f3342
juanmanueloubina <joubina@frba.utn.edu.ar>
2021-07-05 14:36:12 -0300

    Change auth service to throw error if user does not exist

--- my_app/api/builder.py
+++ my_app/api/builder.py
@@ -8,7 +8,8 @@ from my_app.api.repositories import CampaignRepository, PledgeRepository, S3Repo
     GoogleRepository, PrinterRepository, OrderRepository, UserRepository
 from my_app.api.services import CampaignService, PledgeService, OrderService, AuthService
 from my_app.api.utils.token_manager import TokenManager
-from my_app.settings import AWS_BUCKET_NAME, SENDER_EMAIL, GOOGLE_CLIENT_ID, GOOGLE_AUTH_FALLBACK_URL, JWT_SECRET_KEY
+from my_app.settings import AWS_BUCKET_NAME, SENDER_EMAIL, GOOGLE_CLIENT_ID, GOOGLE_AUTH_FALLBACK_URL, JWT_SECRET_KEY, \
+    ENV
 
 
 def inject_controllers(app, db):
@@ -56,7 +57,8 @@ def build_auth_controller(db, executor):
     token_manager = TokenManager(JWT_SECRET_KEY)
     auth_service = AuthService(google_repository, user_repository, token_manager)
 
-    executor.submit(google_repository.warm_up)
+    if ENV != "testing":
+        executor.submit(google_repository.warm_up)
 
     return AuthController(auth_service)
 
