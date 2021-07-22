9b453cf076c39821614cce4d9b6f58dba20a49c1
juanmanueloubina <joubina@frba.utn.edu.ar>
2021-07-21 23:02:26 -0300

    Fix services tests

--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -8,93 +8,91 @@ from my_app.api.exceptions import BusinessException
 from my_app.api.services import UserService
 from tests.test_utils.mock_entities import MOCK_PRINTER, MOCK_PRINTER_PROTOTYPE, MOCK_BUYER, MOCK_BUYER_PROTOTYPE
 
-mock_user_repository = MagicMock()
-user_service = UserService(mock_user_repository)
-
 
 class TestUserService(unittest.TestCase):
 
     def setUp(self):
-        mock_user_repository.reset_mock()
+        self.mock_user_repository = MagicMock()
+        self.user_service = UserService(self.mock_user_repository)
 
     def test_create_printer_returns_printer(self):
-        mock_user_repository.is_user_name_in_use.return_value = False
-        mock_user_repository.is_email_in_use.return_value = False
-        mock_user_repository.create_printer.return_value = MOCK_PRINTER
+        self.mock_user_repository.is_user_name_in_use.return_value = False
+        self.mock_user_repository.is_email_in_use.return_value = False
+        self.mock_user_repository.create_printer.return_value = MOCK_PRINTER
 
-        response = user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
+        response = self.user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
 
         assert isinstance(response, Printer)
-        mock_user_repository.is_user_name_in_use.assert_called_once()
-        mock_user_repository.is_email_in_use.assert_called_once()
-        mock_user_repository.create_printer.assert_called_once_with(MOCK_PRINTER_PROTOTYPE)
+        self.mock_user_repository.is_user_name_in_use.assert_called_once()
+        self.mock_user_repository.is_email_in_use.assert_called_once()
+        self.mock_user_repository.create_printer.assert_called_once_with(MOCK_PRINTER_PROTOTYPE)
 
     def test_create_printer_raises_error_when_user_name_already_in_use(self):
-        mock_user_repository.is_user_name_in_use.return_value = True
+        self.mock_user_repository.is_user_name_in_use.return_value = True
 
         with pytest.raises(BusinessException):
-            user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
+            self.user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
 
     def test_create_printer_raises_error_when_email_already_in_use(self):
-        mock_user_repository.is_user_name_in_use.return_value = False
-        mock_user_repository.is_email_in_use.return_value = True
+        self.mock_user_repository.is_user_name_in_use.return_value = False
+        self.mock_user_repository.is_email_in_use.return_value = True
 
         with pytest.raises(BusinessException):
-            user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
+            self.user_service.create_printer(MOCK_PRINTER_PROTOTYPE)
 
     def test_create_buyer_returns_buyer(self):
-        mock_user_repository.is_user_name_in_use.return_value = False
-        mock_user_repository.is_email_in_use.return_value = False
-        mock_user_repository.create_buyer.return_value = MOCK_BUYER
+        self.mock_user_repository.is_user_name_in_use.return_value = False
+        self.mock_user_repository.is_email_in_use.return_value = False
+        self.mock_user_repository.create_buyer.return_value = MOCK_BUYER
 
-        response = user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
+        response = self.user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
 
         assert isinstance(response, Buyer)
-        mock_user_repository.is_user_name_in_use.assert_called_once()
-        mock_user_repository.is_email_in_use.assert_called_once()
-        mock_user_repository.create_buyer.assert_called_once_with(MOCK_BUYER_PROTOTYPE)
+        self.mock_user_repository.is_user_name_in_use.assert_called_once()
+        self.mock_user_repository.is_email_in_use.assert_called_once()
+        self.mock_user_repository.create_buyer.assert_called_once_with(MOCK_BUYER_PROTOTYPE)
 
     def test_create_buyer_raises_error_when_user_name_already_in_use(self):
-        mock_user_repository.is_user_name_in_use.return_value = True
+        self.mock_user_repository.is_user_name_in_use.return_value = True
 
         with pytest.raises(BusinessException):
-            user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
+            self.user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
 
     def test_create_buyer_raises_error_when_email_already_in_use(self):
-        mock_user_repository.is_user_name_in_use.return_value = False
-        mock_user_repository.is_email_in_use.return_value = True
+        self.mock_user_repository.is_user_name_in_use.return_value = False
+        self.mock_user_repository.is_email_in_use.return_value = True
 
         with pytest.raises(BusinessException):
-            user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
+            self.user_service.create_buyer(MOCK_BUYER_PROTOTYPE)
 
     def test_get_printer_by_email_returns_printer(self):
-        mock_user_repository.get_printer_by_email.return_value = MOCK_PRINTER
+        self.mock_user_repository.get_printer_by_email.return_value = MOCK_PRINTER
 
-        response = user_service.get_printer_by_email(MOCK_PRINTER.email)
+        response = self.user_service.get_printer_by_email(MOCK_PRINTER.email)
 
         assert isinstance(response, Printer)
-        mock_user_repository.get_printer_by_email.assert_called_once_with(MOCK_PRINTER.email)
+        self.mock_user_repository.get_printer_by_email.assert_called_once_with(MOCK_PRINTER.email)
 
     def test_get_buyer_by_email_returns_printer(self):
-        mock_user_repository.get_buyer_by_email.return_value = MOCK_BUYER
+        self.mock_user_repository.get_buyer_by_email.return_value = MOCK_BUYER
 
-        response = user_service.get_buyer_by_email(MOCK_BUYER.email)
+        response = self.user_service.get_buyer_by_email(MOCK_BUYER.email)
 
         assert isinstance(response, Buyer)
-        mock_user_repository.get_buyer_by_email.assert_called_once_with(MOCK_BUYER.email)
+        self.mock_user_repository.get_buyer_by_email.assert_called_once_with(MOCK_BUYER.email)
 
     def test_update_printer_returns_printer(self):
-        mock_user_repository.update_printer.return_value = MOCK_PRINTER
+        self.mock_user_repository.update_printer.return_value = MOCK_PRINTER
 
-        response = user_service.update_printer(1, MOCK_PRINTER_PROTOTYPE)
+        response = self.user_service.update_printer(1, MOCK_PRINTER_PROTOTYPE)
 
         assert isinstance(response, Printer)
-        mock_user_repository.update_printer.assert_called_once_with(1, MOCK_PRINTER_PROTOTYPE)
+        self.mock_user_repository.update_printer.assert_called_once_with(1, MOCK_PRINTER_PROTOTYPE)
 
     def test_update_buyer_returns_printer(self):
-        mock_user_repository.update_buyer.return_value = MOCK_BUYER
+        self.mock_user_repository.update_buyer.return_value = MOCK_BUYER
 
-        response = user_service.update_buyer(1, MOCK_BUYER_PROTOTYPE)
+        response = self.user_service.update_buyer(1, MOCK_BUYER_PROTOTYPE)
 
         assert isinstance(response, Buyer)
-        mock_user_repository.update_buyer.assert_called_once_with(1, MOCK_BUYER_PROTOTYPE)
+        self.mock_user_repository.update_buyer.assert_called_once_with(1, MOCK_BUYER_PROTOTYPE)
